eea7d4388163803aed2fe4a28962a0b97d9cee8a
titan <flameplayz937@gmail.com>
2026-01-18 13:32:38 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,11 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
-import TurnstileGate from "./components/turnstile/TurnstileGate.jsx";
 
 createRoot(document.getElementById("root")).render(
   <LanguageProvider>
     <BrowserRouter>
-      <TurnstileGate siteKey="0x4AAAAAAB8wURRUa25hmamv">
-        <App />
-      </TurnstileGate>
+      <App />
     </BrowserRouter>
   </LanguageProvider>
 );
